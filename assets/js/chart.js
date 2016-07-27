fdc4c70ea41c182dd84b66886fb07e268989264d
ahruschka <ahruschka1@student.gsu.edu>
2016-07-27 15:53:33 -0400

    changes

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -3,15 +3,48 @@ var dataName;
 var timeStamp;
 var startDate;
 var dataArray;
-var hourArray = new Array;
+var spanArray = new Array;
+var span = document.getElementById("span").value;
+document.getElementById("spanValue").innerHTML=span;
+var buffer = document.getElementById("buffer").value;
+document.getElementById("bufferValue").innerHTML=buffer;
+var sma = simple_moving_averager(buffer*60/span);
+var smaArray = new Array;
 
+function simple_moving_averager(period){
+  smaArray = [];
+  var x;
+  for (var i = 0; i < spanArray.length-1;i++){
+    if(i<buffer){
+      x = 0;
+      for(var j = 0;j<i;j++){
+        x = x + spanArray[i-j];
+      }
+      smaArray[i] = (x/i);
+    }else{
+      x = 0;
+      for(var j = 0;j<buffer;j++){
+        x = x + spanArray[i-j];
+      }
+      smaArray[i] = (x/buffer);
+    }
+  }
+}
 function read(textFile){
   var xhr=new XMLHttpRequest;
   xhr.open('GET',textFile);
   xhr.onload=show;
-  xhr.send()
+  xhr.send();
 }
-
+function spanBufferChanged(){
+  buffer = document.getElementById("buffer").value;
+  document.getElementById("bufferValue").innerHTML=buffer;
+  span = document.getElementById("span").value;
+  document.getElementById("spanValue").innerHTML=span;
+  formatData(span);
+  sma = simple_moving_averager(buffer*60/span);
+  drawChart();
+}
 function show(){
   //var pre=document.createElement('pre');
   dataSet=this.response.split("\r\n");
@@ -19,21 +52,28 @@ function show(){
   var dateArray= dataName[0].split("_");
   timeStamp = new Date(dateArray[0],dateArray[1]-1,dateArray[2],dateArray[4],dateArray[5],dateArray[6]);
   startDate = new Date(dateArray[0],dateArray[1]-1,dateArray[2],dateArray[4],dateArray[5],dateArray[6]);
-
   //Removes counts before first hour
-  // if((59-startDate.getMinutes()) != 0){
-  //   var extras = 59-startDate.getMinutes();
-  //   for (var i = 0; i < extras; i++) {
-  //     timeStamp.setTime(timeStamp.getTime()+60000);
-  //   }
-  // }
+  if((59-startDate.getMinutes()) != 0){
+    var extras = 59-startDate.getMinutes();
+    for (var i = 0; i < extras; i++) {
+      timeStamp.setTime(timeStamp.getTime()+60000);
+    }
+  }
+  formatData(span);
+  sma = simple_moving_averager(buffer*60/span);
+
+  //pre.textContent=this.response.split("\r\n");
+  // document.body.appendChild(pre)
+}
+function formatData(x) {
   //Andrew Test
+  spanArray = [];
   var total = 0;
   var i;
   var c = 0;
   for(i=0;i<(dataSet.length)-1;i++){
-    if(c==60){
-      hourArray.push(total/60);
+    if(c==x){
+      spanArray.push(total/x);
       total = 0;
       c = 0;
     }else{
@@ -41,27 +81,8 @@ function show(){
       c++;
     }
   }
-
-  //pre.textContent=this.response.split("\r\n");
-  // document.body.appendChild(pre)
-}
-function simple_moving_averager(period) {
-  var nums = [];
-  return function(num) {
-    nums.push(num);
-    if (nums.length > period)
-    nums.splice(0,1);  // remove the first element of the array
-    var sum = 0;
-    for (var i in nums)
-    sum += nums[i];
-    var n = period;
-    if (nums.length < period)
-    n = nums.length;
-    return(sum/n);
-  }
 }
 
-var sma = simple_moving_averager(6);
 
 read("http://"+location.hostname+"/2016_07_18__14_45_30.log");
 google.charts.load('current', {'packages':['corechart']});
@@ -69,16 +90,18 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
   var data = new google.visualization.DataTable();
+  var timeAdvance = new Date();
+  timeAdvance.setTime(timeStamp.getTime());
   data.addColumn('date', 'Time');
   data.addColumn('number', 'Counts');
   data.addColumn('number','Rolling Average')
 
-  for (i = 0; i < hourArray.length-1; i++) {
-    timeStamp.setTime(timeStamp.getTime()+3600000);
+  for (i = 0; i < spanArray.length-1; i++) {
+    timeAdvance.setTime(timeAdvance.getTime()+(span*60000));
     data.addRow([
-      new Date(timeStamp.getTime()),
-      hourArray[i],
-      sma(Number(hourArray[i]))
+      new Date(timeAdvance.getTime()),
+      spanArray[i],
+      smaArray[i]
     ]);
   }
 
